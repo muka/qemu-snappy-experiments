4656b6650ad7d84c4134c46fa3dc6330d03057f6
Luca Capra <luca.capra@gmail.com>
2016-01-22 14:05:39 +0100

    correct run

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
+
+# TODO: Fix networking
+
+#  -netdev tap,id=t0,ifname=tap0,script=no,downscript=no \
+#  -net nic,model=lan9118,netdev=t0 \
+
 qemu-system-arm -kernel arm-vmlinux \
  -append "root=/dev/mmcblk0p2 rw console=tty0" \
  -m 1G -cpu cortex-a9 -M vexpress-a9 \
  -netdev tap,id=t0,ifname=tap0,script=no,downscript=no \
  -net nic,model=lan9118,netdev=t0 \
- -sd ubuntu-15.04-snappy-armhf-raspi2.img 
-
+ -sd ubuntu-15.04-snappy-armhf-raspi2.img
